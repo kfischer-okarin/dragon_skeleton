758e91473e388a7b33ad733913e4f7adddffde2a
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-12-12 16:28:25 +0900

    Fix requires to be smaug compatible

--- app/main.rb
+++ app/main.rb
@@ -1,4 +1,7 @@
-require 'lib/dr_base_framework.rb'
+require 'lib/dr_base_framework/animations.rb'
+require 'lib/dr_base_framework/animations/asesprite_json.rb'
+require 'lib/dr_base_framework/animated_sprite.rb'
+
 
 # Placeholder file for unit tests
 def tick(_args); end
--- lib/dr_base_framework.rb
+++
@@ -1,3 +0,0 @@
-require 'lib/dr_base_framework/animations.rb'
-require 'lib/dr_base_framework/animations/asesprite_json.rb'
-require 'lib/dr_base_framework/animated_sprite.rb'
