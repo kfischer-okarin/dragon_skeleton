31abedc00c7f27f3b2f3204dcc3d0e079574f035
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-01-24 21:30:02 +0900

    feat: Implement Screen to screen position and pixel rect

--- lib/dragon_skeleton/screen.rb
+++ lib/dragon_skeleton/screen.rb
@@ -25,6 +25,22 @@ module DragonSkeleton
       def sprite(screen)
         screen.slice(:x, :y, :w, :h).sprite!(path: :screen)
       end
+
+      def to_screen_position(screen, position)
+        {
+          x: (position.x - screen[:x]).idiv(screen[:scale]),
+          y: (position.y - screen[:y]).idiv(screen[:scale])
+        }
+      end
+
+      def pixel_rect(screen, screen_position)
+        {
+          x: screen[:x] + (screen_position.x * screen[:scale]),
+          y: screen[:y] + (screen_position.y * screen[:scale]),
+          w: screen[:scale],
+          h: screen[:scale]
+        }
+      end
     end
   end
 end
--- tests/lib/dragon_skeleton/screen.rb
+++ tests/lib/dragon_skeleton/screen.rb
@@ -35,3 +35,19 @@ def test_screen_sprite(_args, assert)
     assert.equal! sprite, test_case[:expected_sprite]
   end
 end
+
+def test_screen_to_screen_position(_args, assert)
+  screen = Screen.with_resolution(72, 72) # scale: 10, offset: 280
+
+  screen_position = Screen.to_screen_position(screen, { x: 385, y: 55 })
+
+  assert.equal! screen_position, { x: 10, y: 5 }
+end
+
+def test_screen_pixel_rect(_args, assert)
+  screen = Screen.with_resolution(72, 72) # scale: 10, offset: 280
+
+  pixel_rect = Screen.pixel_rect(screen, { x: 10, y: 5 })
+
+  assert.equal! pixel_rect, { x: 380, y: 50, w: 10, h: 10 }
+end
