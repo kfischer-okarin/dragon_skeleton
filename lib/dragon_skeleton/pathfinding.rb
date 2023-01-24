c476e8620fcca2fbc8345458a0e2dc2dc621cad8
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-01-24 12:00:06 +0900

    feat: Add chebyshev and euclidean distance too

--- lib/dragon_skeleton/pathfinding.rb
+++ lib/dragon_skeleton/pathfinding.rb
@@ -1,5 +1,7 @@
 module DragonSkeleton
   module Pathfinding
     MANHATTAN_DISTANCE = ->(a, b) { (a[:x] - b[:x]).abs + (a[:y] - b[:y]).abs }
+    CHEBYSHEV_DISTANCE = ->(a, b) { [(a[:x] - b[:x]).abs, (a[:y] - b[:y]).abs].max }
+    EUCLIDEAN_DISTANCE = ->(a, b) { Math.sqrt((a[:x] - b[:x])**2 + (a[:y] - b[:y])**2) }
   end
 end
--- tests/lib/dragon_skeleton/pathfinding.rb
+++ tests/lib/dragon_skeleton/pathfinding.rb
@@ -5,3 +5,15 @@ def test_pathfinding_manhattan_distance(_args, assert)
 
   assert.equal! distance, 4
 end
+
+def test_pathfinding_chebyshev_distance(_args, assert)
+  distance = Pathfinding::CHEBYSHEV_DISTANCE.call({ x: 1, y: 1 }, { x: 3, y: 3 })
+
+  assert.equal! distance, 2
+end
+
+def test_pathfinding_euclidean_distance(_args, assert)
+  distance = Pathfinding::EUCLIDEAN_DISTANCE.call({ x: 1, y: 1 }, { x: 4, y: 5 })
+
+  assert.equal! distance, 5
+end
