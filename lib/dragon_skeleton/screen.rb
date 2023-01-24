17b9959e801e7c8cff782b0977c1a36c9c97f41d
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-01-24 21:45:50 +0900

    docs: Add Documentation for Screen module

--- lib/dragon_skeleton/screen.rb
+++ lib/dragon_skeleton/screen.rb
@@ -1,6 +1,31 @@
 module DragonSkeleton
+  # Contains methods for working with a low-resolution screen.
+  #
+  # First, you need to create a screen object via ::with_resolution.
+  #
+  #   screen = Screen.with_resolution(320, 180)
+  #
+  # Then you must prepare render target with ::build_render_target to draw to the screen.
+  #
+  #   render_target = Screen.build_render_target(args, screen)
+  #   render_target.primitives << { x: 20, y: 20, w: 32, h: 32, path: 'character.png' }.sprite!
+  #
+  # Finally, you need to draw the screen to the main render area using ::sprite.
+  #
+  #   args.outputs.primitives << Screen.sprite(screen)
   module Screen
     class << self
+      # Creates a screen object with the given resolution.
+      #
+      # The scale of the screen (2 for example meaning the screen is rendered at
+      # double size) is automatically calculated from the resolution to fit into
+      # 1280x720.
+      #
+      # The position of the screen is automatically calculated to be centered on the
+      # display.
+      #
+      # Both the scale and position can be overridden via the +scale+ and
+      # +render_position+ keyword arguments.
       def with_resolution(x_resolution, y_resolution, scale: nil, render_position: nil)
         scale ||= [1280.idiv(x_resolution), 720.idiv(y_resolution)].min
         w = x_resolution * scale
@@ -15,6 +40,7 @@ module DragonSkeleton
         )
       end
 
+      # Creates a render target used to render to the given screen.
       def build_render_target(args, screen)
         result = args.outputs[:screen]
         result.width = screen[:x_resolution]
@@ -22,10 +48,12 @@ module DragonSkeleton
         result
       end
 
+      # Creates a sprite that is used to draw the given screen to the display.
       def sprite(screen)
         screen.slice(:x, :y, :w, :h).sprite!(path: :screen)
       end
 
+      # Converts a display position to coordinates on the given screen.
       def to_screen_position(screen, position)
         {
           x: (position.x - screen[:x]).idiv(screen[:scale]),
@@ -33,6 +61,8 @@ module DragonSkeleton
         }
       end
 
+      # Returns the rectangle on the display where the pixel at the given screen
+      # position is drawn.
       def pixel_rect(screen, screen_position)
         {
           x: screen[:x] + (screen_position.x * screen[:scale]),
