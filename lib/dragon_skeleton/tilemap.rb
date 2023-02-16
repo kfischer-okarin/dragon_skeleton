de61e662c07f9d72f1a77fcfca146363de5f9f20
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-02-16 20:44:41 +0900

    feat: Write first real tilemap implementation

--- lib/dragon_skeleton/tilemap.rb
+++ lib/dragon_skeleton/tilemap.rb
@@ -1,21 +1,52 @@
 module DragonSkeleton
   class Tilemap
     def initialize(x:, y:, cell_w:, cell_h:, grid_w:, grid_h:)
+      @grid_w = grid_w
+      @tiles = grid_h.times.flat_map { |grid_y|
+        grid_w.times.map { |grid_x|
+          [
+            x + grid_x * cell_w,
+            y + grid_y * cell_h,
+            nil
+          ].tap { |tile| tile.extend(Tile) }
+        }
+      }
+      @primitive = RenderedPrimitive.new(@tiles, cell_w, cell_h)
     end
 
     def [](x, y)
-      {}
+      @tiles[y * @grid_w + x]
     end
 
     def render(outputs)
-      outputs.primitives << RenderedPrimitive.new
+      outputs.primitives << @primitive
+    end
+
+    module Tile
+      def path
+        self[2]
+      end
+
+      def path=(path)
+        self[2] = path
+      end
     end
 
     class RenderedPrimitive
+      def initialize(tiles, cell_w, cell_h)
+        @tiles = tiles
+        @cell_w = cell_w
+        @cell_h = cell_h
+      end
+
       def draw_override(ffi_draw)
-        [[0, 0, 1], [100, 0, 2], [0, 100, 3], [100, 100, 4], [0, 200, 5], [100, 200, 6]].each do |x, y, sprite_no|
-          ffi_draw.draw_sprite_4 x, y, 100, 100,
-                                 "sprites/#{sprite_no}.png",
+        w = @cell_w
+        h = @cell_h
+
+        @tiles.each do |tile|
+          x, y, path = tile
+          ffi_draw.draw_sprite_4 x, y, w, h,
+                                 path,
                                  nil, # angle
                                  nil, nil, nil, nil, # a, r, g, b
                                  nil, nil, nil, nil, # tile_x, tile_y, tile_w, tile_h
