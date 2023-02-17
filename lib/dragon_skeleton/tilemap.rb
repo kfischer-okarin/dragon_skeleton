95d385fcd5db6c5bc2e80437f7b8aefdc13778d5
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-02-17 12:31:12 +0900

    feat: Allow Tilemap offset to be changed

--- lib/dragon_skeleton/tilemap.rb
+++ lib/dragon_skeleton/tilemap.rb
@@ -13,15 +13,15 @@ module DragonSkeleton
       @tiles = grid_h.times.flat_map { |grid_y|
         grid_w.times.map { |grid_x|
           [
-            x + grid_x * cell_w,
-            y + grid_y * cell_h,
+            grid_x * cell_w,
+            grid_y * cell_h,
             nil, # path
             nil, nil, nil, nil, # r, g, b, a
             nil, nil, nil, nil  # tile_x, tile_y, tile_w, tile_h
           ].tap { |tile| tile.extend(Tile) }
         }
       }
-      @primitive = RenderedPrimitive.new(@tiles, cell_w, cell_h)
+      @primitive = RenderedPrimitive.new(@tiles, self)
     end
 
     def [](x, y)
@@ -44,10 +44,9 @@ module DragonSkeleton
     end
 
     class RenderedPrimitive
-      def initialize(tiles, cell_w, cell_h)
+      def initialize(tiles, tilemap)
         @tiles = tiles
-        @cell_w = cell_w
-        @cell_h = cell_h
+        @tilemap = tilemap
       end
 
       def primitive_marker
@@ -55,14 +54,16 @@ module DragonSkeleton
       end
 
       def draw_override(ffi_draw)
-        w = @cell_w
-        h = @cell_h
+        origin_x = @tilemap.x
+        origin_y = @tilemap.y
+        w = @tilemap.cell_w
+        h = @tilemap.cell_h
         tile_count = @tiles.size
         index = 0
 
         while index < tile_count
           x, y, path, r, g, b, a, tile_x, tile_y, tile_w, tile_h = @tiles[index]
-          ffi_draw.draw_sprite_4 x, y, w, h,
+          ffi_draw.draw_sprite_4 origin_x + x, origin_y + y, w, h,
                                  path,
                                  nil, # angle
                                  a, r, g, b,
