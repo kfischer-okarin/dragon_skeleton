97437460cd728a6a9f2c84d740fb89e92c9069e8
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-02-17 12:28:36 +0900

    tests: Test Tilemap x, y

--- tests/lib/dragon_skeleton/tilemap.rb
+++ tests/lib/dragon_skeleton/tilemap.rb
@@ -1,7 +1,7 @@
 DragonSkeleton.add_to_top_level_namespace
 
 def test_tilemap_render(args, assert)
-  tilemap = Tilemap.new(x: 0, y: 0, cell_w: 100, cell_h: 100, grid_w: 2, grid_h: 3)
+  tilemap = Tilemap.new(x: 50, y: 50, cell_w: 100, cell_h: 100, grid_w: 2, grid_h: 3)
   tilemap[0, 0].path = 'tile.png'
   tilemap[1, 0].r = 255
   tilemap[0, 1].g = 255
@@ -22,32 +22,32 @@ def test_tilemap_render(args, assert)
     [
       :draw_sprite_4,
       #   x,   y,   w,   h,       path, angle,   a,   r,   g,   b, tile_x, tile_y, tile_w, tile_h, flip_horizontally, flip_vertically, angle_anchor_x, angle_anchor_y, source_x, source_y, source_w, source_h, blendmode_enum
-      [   0,   0, 100, 100, 'tile.png',   nil, nil, nil, nil, nil,    nil,    nil,    nil,    nil,               nil,             nil,            nil,            nil,      nil,      nil,      nil,      nil,            nil]
+      [  50,  50, 100, 100, 'tile.png',   nil, nil, nil, nil, nil,    nil,    nil,    nil,    nil,               nil,             nil,            nil,            nil,      nil,      nil,      nil,      nil,            nil]
     ],
     [
       :draw_sprite_4,
       #   x,   y,   w,   h,       path, angle,   a,   r,   g,   b, tile_x, tile_y, tile_w, tile_h, flip_horizontally, flip_vertically, angle_anchor_x, angle_anchor_y, source_x, source_y, source_w, source_h, blendmode_enum
-      [ 100,   0, 100, 100,        nil,   nil, nil, 255, nil, nil,    nil,    nil,    nil,    nil,               nil,             nil,            nil,            nil,      nil,      nil,      nil,      nil,            nil]
+      [ 150,  50, 100, 100,        nil,   nil, nil, 255, nil, nil,    nil,    nil,    nil,    nil,               nil,             nil,            nil,            nil,      nil,      nil,      nil,      nil,            nil]
     ],
     [
       :draw_sprite_4,
       #   x,   y,   w,   h,       path, angle,   a,   r,   g,   b, tile_x, tile_y, tile_w, tile_h, flip_horizontally, flip_vertically, angle_anchor_x, angle_anchor_y, source_x, source_y, source_w, source_h, blendmode_enum
-      [   0, 100, 100, 100,        nil,   nil, nil, nil, 255, nil,    nil,    nil,    nil,    nil,               nil,             nil,            nil,            nil,      nil,      nil,      nil,      nil,            nil]
+      [  50, 150, 100, 100,        nil,   nil, nil, nil, 255, nil,    nil,    nil,    nil,    nil,               nil,             nil,            nil,            nil,      nil,      nil,      nil,      nil,            nil]
     ],
     [
       :draw_sprite_4,
       #   x,   y,   w,   h,       path, angle,   a,   r,   g,   b, tile_x, tile_y, tile_w, tile_h, flip_horizontally, flip_vertically, angle_anchor_x, angle_anchor_y, source_x, source_y, source_w, source_h, blendmode_enum
-      [ 100, 100, 100, 100,        nil,   nil, nil, nil, nil, 255,    nil,    nil,    nil,    nil,               nil,             nil,            nil,            nil,      nil,      nil,      nil,      nil,            nil]
+      [ 150, 150, 100, 100,        nil,   nil, nil, nil, nil, 255,    nil,    nil,    nil,    nil,               nil,             nil,            nil,            nil,      nil,      nil,      nil,      nil,            nil]
     ],
     [
       :draw_sprite_4,
       #   x,   y,   w,   h,       path, angle,   a,   r,   g,   b, tile_x, tile_y, tile_w, tile_h, flip_horizontally, flip_vertically, angle_anchor_x, angle_anchor_y, source_x, source_y, source_w, source_h, blendmode_enum
-      [   0, 200, 100, 100,        nil,   nil, 255, nil, nil, nil,    nil,    nil,    nil,    nil,               nil,             nil,            nil,            nil,      nil,      nil,      nil,      nil,            nil]
+      [  50, 250, 100, 100,        nil,   nil, 255, nil, nil, nil,    nil,    nil,    nil,    nil,               nil,             nil,            nil,            nil,      nil,      nil,      nil,      nil,            nil]
     ],
     [
       :draw_sprite_4,
       #   x,   y,   w,   h,            path, angle,   a,   r,   g,   b, tile_x, tile_y, tile_w, tile_h, flip_horizontally, flip_vertically, angle_anchor_x, angle_anchor_y, source_x, source_y, source_w, source_h, blendmode_enum
-      [ 100, 200, 100, 100,             nil,   nil, nil, nil, nil, nil,    nil,    nil,    nil,    nil,               nil,             nil,            nil,            nil,      nil,      nil,      nil,      nil,            nil]
+      [ 150, 250, 100, 100,             nil,   nil, nil, nil, nil, nil,    nil,    nil,    nil,    nil,               nil,             nil,            nil,            nil,      nil,      nil,      nil,      nil,            nil]
     ]
   ]
   # rubocop:enable all
