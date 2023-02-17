604a8ce50b8696e49d3f768799db6cbc010d4d99
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-02-17 13:00:53 +0900

    tests: Fix indent

--- tests/lib/dragon_skeleton/tilemap.rb
+++ tests/lib/dragon_skeleton/tilemap.rb
@@ -46,8 +46,8 @@ def test_tilemap_render(args, assert)
     ],
     [
       :draw_sprite_4,
-      #   x,   y,   w,   h,            path, angle,   a,   r,   g,   b, tile_x, tile_y, tile_w, tile_h, flip_horizontally, flip_vertically, angle_anchor_x, angle_anchor_y, source_x, source_y, source_w, source_h, blendmode_enum
-      [ 150, 250, 100, 100,             nil,   nil, nil, nil, nil, nil,    nil,    nil,    nil,    nil,               nil,             nil,            nil,            nil,      nil,      nil,      nil,      nil,            nil]
+      #   x,   y,   w,   h,       path, angle,   a,   r,   g,   b, tile_x, tile_y, tile_w, tile_h, flip_horizontally, flip_vertically, angle_anchor_x, angle_anchor_y, source_x, source_y, source_w, source_h, blendmode_enum
+      [ 150, 250, 100, 100,        nil,   nil, nil, nil, nil, nil,    nil,    nil,    nil,    nil,               nil,             nil,            nil,            nil,      nil,      nil,      nil,      nil,            nil]
     ]
   ]
   # rubocop:enable all
